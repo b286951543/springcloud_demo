cd55af0423484ce410524fb1db99ac921323d5af
b286951543 <892153580@qq.com>
2022-09-09 17:03:45 +0800

    add

--- springcloud-nacos/src/main/java/com/nacos/controller/ConfigController.java
+++ springcloud-nacos/src/main/java/com/nacos/controller/ConfigController.java
@@ -11,8 +11,20 @@ public class ConfigController {
     @Value("${jzx.name:1234}") // 取不到配置时，使用 1234
     private String appName;
 
+    @Value("${nacos1.name:1234}") // 取不到配置时，使用 1234
+    private String name1;
+
+    @Value("${nacos2.name:1234}") // 取不到配置时，使用 1234
+    private String name2;
+
+    @Value("${shared01.name:1234}") // 取不到配置时，使用 1234
+    private String sharedName1;
+
+    @Value("${shared02.name:1234}") // 取不到配置时，使用 1234
+    private String sharedName2;
+
     @GetMapping("/api/appInfo")
     public String appInfo() {
-        return "appName: " + appName;
+        return "appName: " + appName + "," + name1 + "," + name2 + "。 " + sharedName1 + "," + sharedName2;
     }
 }
